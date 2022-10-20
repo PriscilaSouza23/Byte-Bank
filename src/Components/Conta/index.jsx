041237c4c7cb97a6a6f78a2c29a587ebc18ece2a
PriscilaLSouza <priscila.souza@cmosdrake.com.br>
2022-10-20 17:41:59 -0300

    feature project ByteBank

---
+++ src/Components/Conta/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+import privado from "../../assets/images/privado.svg";
+import olho_icone from "../../assets/images/olho.svg";
+import dinheiro from "../../assets/images/dinheiro.svg";
+
+import { Icone } from "../UI/index";
+
+const Conta = () => {
+  const [toggleState, untoggle] = useState(true);
+
+  const toggleHandler = () => {
+    untoggle((toggleState) => !toggleState);
+  };
+
+  return (
+    <div className="box">
+      <h2>Conta</h2>
+      <div style={{ fontSize: "26px", padding: "20px 0" }}>
+        Saldo disponível{" "}
+        <span>
+          <Icone src={dinheiro} alt="Ícone Saldo" />
+        </span>
+        {toggleState ? (
+          <div className="saldo">
+            <span className="detalhe">R$</span> 0,00{" "}
+          </div>
+        ) : null}
+      </div>
+
+      <button className="btn" onClick={toggleHandler}>
+        <Icone
+          style={{ marginTop: "2px" }}
+          src={toggleState ? privado : olho_icone}
+          alt="Privacidade do Saldo"
+        />
+      </button>
+    </div>
+  );
+};
+
+export default Conta;
